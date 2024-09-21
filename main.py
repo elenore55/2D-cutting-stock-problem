2e395f028665b7adef6ec129c92ea21bf9c1706f
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2024-09-21 17:53:22 +0200

    Implemented crossover and mutation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from with_operators import generate_initial_population, calculate_height
+from with_operators import generate_initial_population, calculate_height, crossover_pmx
 
 if __name__ == '__main__':
     initial = generate_initial_population()
     print(min(chromosome.cost for chromosome in initial))
+    print(crossover_pmx([1, 4, 2, 5, 3], [4, 2, 5, 1, 3]))
     # for chromosome in initial:
     #     print(chromosome, end=' ')
     #     print(calculate_height(chromosome.chromosome))
--- with_operators.py
+++ with_operators.py
@@ -20,7 +20,7 @@ POPULATION_SIZE = 30
 CHROMOSOME_LEN = 2 * NUM_PIECES - 1
 MAX_ITERS = 1000
 ELITISM = 3
-MUTATION_RATE = 0.2
+MUTATION_RATE = 0.5
 
 
 class BoundingBox(object):
@@ -37,17 +37,82 @@ def main():
 
     for _ in range(MAX_ITERS):
         next_population = []
-
+        # selection of parents
         population = next_population
 
 
 def crossover(parent1: list, parent2: list):
+    pieces1 = [gene for gene in parent1 if gene not in OPERATORS]
+    pieces2 = [gene for gene in parent2 if gene not in OPERATORS]
+    child_pieces1, child_pieces2 = crossover_pmx(pieces1, pieces2)
+    child1 = []
+    child2 = []
+    pieces_counter = 0
+    for gene in parent1:
+        if gene in OPERATORS:
+            child1.append(gene)
+        else:
+            child1.append(child_pieces1[pieces_counter])
+            pieces_counter += 1
+    pieces_counter = 0
+    for gene in parent2:
+        if gene in OPERATORS:
+            child2.append(gene)
+        else:
+            child2.append(child_pieces2[pieces_counter])
+            pieces_counter += 1
+    return child1, child2
 
-    pass
+
+def crossover_pmx(parent1: list, parent2: list):
+    assert len(parent1) == len(parent2)
+    i1, i2 = random.sample(range(1, len(parent1)), 2)
+    if i1 > i2:
+        i1, i2 = i2, i1
+    middle1 = parent1[i1:i2]
+    middle2 = parent2[i1:i2]
+    mapping = {}
+    for i in range(len(middle1)):
+        mapping[middle1[i]] = middle2[i]
+    mapping[middle2[len(middle2) - 1]] = middle1[0]
+
+    child1 = parent2[:i1] + middle1 + parent2[i2:]
+    child2 = parent1[:i1] + middle2 + parent1[i2:]
+    for i in range(i1):
+        while child1[i] in mapping:
+            if child1.count(child1[i]) == 1:
+                break
+            child1[i] = mapping[child1[i]]
+        while child2[i] in mapping:
+            if child2.count(child2[i]) == 1:
+                break
+            child2[i] = mapping[child2[i]]
+    for i in range(i2, len(child1)):
+        while child1[i] in mapping:
+            if child1.count(child1[i]) == 1:
+                break
+            child1[i] = mapping[child1[i]]
+        while child2[i] in mapping:
+            if child2.count(child2[i]) == 1:
+                break
+            child2[i] = mapping[child2[i]]
+    return child1, child2
 
 
-def mutate(chromosome: list):
-    pass
+def mutate(chromosome: list) -> list:
+    if random.random() < MUTATION_RATE:
+        i1, i2 = random.sample(range(CHROMOSOME_LEN), 2)
+        p1 = chromosome[i1]
+        p2 = chromosome[i2]
+        if p1 not in OPERATORS and p2 in OPERATORS:
+            chromosome_copy = copy.deepcopy(chromosome)
+            chromosome_copy[i1], chromosome_copy[i2] = p2, p1
+            if is_valid(chromosome_copy):
+                return chromosome_copy
+        else:
+            chromosome[i1], chromosome[i2] = p2, p1
+    return chromosome
+
 
 def generate_initial_population() -> list[Chromosome]:
     population = [generate_chromosome() for _ in range(POPULATION_SIZE)]
@@ -129,3 +194,16 @@ def calculate_bounding_box(chromosome) -> BoundingBox:
 
 def evaluate_cost(chromosome: list) -> float:
     return calculate_width(chromosome)
+
+
+def is_valid(chromosome: list) -> bool:
+    piece_count = 0
+    operator_count = 1
+    for gene in chromosome:
+        if gene not in OPERATORS:
+            piece_count += 1
+        else:
+            if operator_count > piece_count - 1:
+                return False
+            operator_count += 1
+    return True
